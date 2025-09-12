bbb5359cd3a48cea91e83db342e65c8068b05d23
Shamil Khaibullov <shamil@shamil.dev>
2025-09-12 21:01:08 +0100

    feat(ui): add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-import { Post } from './components/Post.jsx'
+import { PostList } from './components/PostList.jsx'
+
+const posts = [
+  {
+    title: 'Full-Stack React Projects',
+    contents: "Let's become full-stack developers!",
+    author: 'Shamil Khaibullov',
+  },
+  { title: 'Hello React!' },
+]
 
 export function App() {
-  return (
-    <Post
-      title='Full-Stack React Projects'
-      contents="Let's become full-stack developers!"
-      author='Shamil Khaibullov'
-    />
-  )
+  return <PostList posts={posts} />
 }
---
+++ src/components/PostFilter.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+
+export function PostFilter({ field }) {
+  return (
+    <div>
+      <label htmlFor={`filter-${field}`}>{field}: </label>
+      <input type='text' name={`filter-${field}`} id={`filter-${field}`} />
+    </div>
+  )
+}
+
+PostFilter.propTypes = {
+  field: PropTypes.string.isRequired,
+}
