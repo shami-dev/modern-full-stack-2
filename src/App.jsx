dcdcfb7abdd3367e985cac0d24ba3d0278222335
Shamil Khaibullov <shamil@shamil.dev>
2025-09-13 14:34:40 +0100

    feat: add and configure Tanstack Query and fetch posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import { CreatePost } from './components/CreatePost.jsx'
-import { PostFilter } from './components/PostFilter.jsx'
-import { PostSorting } from './components/PostSorting.jsx'
-import { PostList } from './components/PostList.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Blog } from './Blog.jsx'
 
-const posts = [
-  {
-    title: 'Full-Stack React Projects',
-    contents: "Let's become full-stack developers!",
-    author: 'Shamil Khaibullov',
-  },
-  { title: 'Hello React!' },
-]
+const queryClient = new QueryClient()
 
 export function App() {
   return (
-    <div style={{ padding: 8 }}>
-      <CreatePost />
-      <br />
-      <hr />
-      Filter by:
-      <PostFilter field='author' />
-      <br />
-      <PostSorting fields={['createdAt', 'updatedAt']} />
-      <hr />
-      <PostList posts={posts} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Blog />
+    </QueryClientProvider>
   )
 }
